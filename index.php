2148b228560dd2b5b78ffc88ab7b730d0b3e601f
mslearn0055 <137442343+mslearn0055@users.noreply.github.com>
2024-08-23 23:45:46 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php 
-echo "Hello mogul- u were really rude ---- actaully  iwas testing this poc since 2 weeks ... today i got the deployment right , so i was excited ---- tc bye ... ";
+echo "Hello mogul- ... ";
 
 ?>
